889b94648b5e54c9e43b3f9dfb2c3584e457ff81
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-02-21 10:24:13 -0700

    Added .addAll method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,5 +17,18 @@ public class Main {
 
         System.out.println(moreApprentices);
 
+        List<String> stringsOnly = new ArrayList<>();
+        stringsOnly.add("House");
+        stringsOnly.add("car");
+        stringsOnly.add(String.valueOf(true));
+        System.out.println(stringsOnly);
+
+        //create arraylist named allStrings with only String type parameter.
+        //add the contents of your stringOnly array list to allStrings
+        //Add another string object to your allStrings at index 2.
+        List<String> allStrings = new ArrayList<>();
+        allStrings.addAll(stringsOnly);
+        allStrings.add(2, "Person");
+        System.out.println(allStrings);
     }
 }
